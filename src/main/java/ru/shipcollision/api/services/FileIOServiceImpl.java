c190e98d9e14100098e14d9288e361639705bade
jpyatachkov <cvkucherov@yandex.ru>
2018-03-08 02:00:54 +0300

    Implemented correct creating directories

--- src/main/java/ru/shipcollision/api/services/FileIOServiceImpl.java
+++ src/main/java/ru/shipcollision/api/services/FileIOServiceImpl.java
@@ -13,51 +13,78 @@ import java.time.LocalDateTime;
 @Service
 public class FileIOServiceImpl implements FileIOService {
 
-    public static final String BASE_PATH = "uploads/";
-
-    private String resolveDirectoryPath(String originalFilename) {
-        final LocalDateTime now = LocalDateTime.now();
-        final String extension = originalFilename.split("\\.")[1];
-        final Path path = Paths.get(String.format("%s%d/%d/%d/",
-                BASE_PATH,
-                now.getYear(),
-                now.getMonthValue(),
-                now.getDayOfMonth()
-        )).toAbsolutePath();
-
-        try {
-            Files.createDirectories(path);
-        } catch (IOException e) {
-            throw new ApiException(String.format("Impossible to create directory %s", path));
-        }
-
-        return String.format("%s/%d%d%d.%s",
-                path.toString(),
-                now.getHour(),
-                now.getMinute(),
-                now.getSecond(),
-                extension
-        );
-    }
+    public static final String BASE_PATH = "uploads";
 
     @Override
     public String saveAndGetPath(MultipartFile file) {
-        final String saveFilePath = resolveDirectoryPath(file.getOriginalFilename());
+        final UploadResourceResolver resolver = new UploadResourceResolver(file.getOriginalFilename());
 
-        try (FileOutputStream out = new FileOutputStream(saveFilePath)) {
+        try (FileOutputStream out = new FileOutputStream(resolver.getSaveFilePath())) {
             out.write(file.getBytes());
             out.close();
         } catch (FileNotFoundException e) {
-            throw new ApiException(String.format("File %s not found", saveFilePath));
+            throw new ApiException(String.format("File %s not found", resolver.getSaveFilePath()));
         } catch (IOException e) {
-            throw new ApiException(String.format("Impossible to save to file %s", saveFilePath));
+            throw new ApiException(String.format("Impossible to save to file %s", resolver.getSaveFilePath()));
         }
 
-        return saveFilePath;
+        return resolver.getResoursePath();
     }
 
     @Override
     public MultipartFile load(String path) {
         return null;
+    }
+
+    private static final class UploadResourceResolver {
+
+        private String filename;
+
+        private String resoursePath;
+
+        private String saveFilePath;
+
+        private UploadResourceResolver(String originalFilename) {
+            final LocalDateTime now = LocalDateTime.now();
+
+            final String fileExtension = originalFilename.split("\\.")[1];
+            filename = String.format(
+                    "%d%d%d.%s",
+                    now.getHour(),
+                    now.getMinute(),
+                    now.getSecond(),
+                    fileExtension
+            );
+
+            resoursePath = String.format(
+                    "%d/%d/%d",
+                    now.getYear(),
+                    now.getMonthValue(),
+                    now.getDayOfMonth()
+            );
+
+            final Path uploadPath = Paths.get(String.format(
+                    "%s/%s",
+                    BASE_PATH,
+                    resoursePath
+            )).toAbsolutePath();
+
+            try {
+                Files.createDirectories(uploadPath);
+            } catch (IOException e) {
+                throw new ApiException(String.format("Impossible to create directory %s", uploadPath));
+            }
+
+            resoursePath = String.format("/%s/%s/%s", BASE_PATH, resoursePath, filename);
+            saveFilePath = String.format("%s/%s", uploadPath.toString(), filename);
+        }
+
+        public String getResoursePath() {
+            return resoursePath;
+        }
+
+        public String getSaveFilePath() {
+            return saveFilePath;
+        }
     }
 }
