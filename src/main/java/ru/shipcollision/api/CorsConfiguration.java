41a9710e6c36f1f44449fb7ed205fb7f3c3910e8
George <gabolaev98@gmail.com>
2018-03-07 17:31:59 +0300

    heroku origin url switched

--- src/main/java/ru/shipcollision/api/CorsConfiguration.java
+++ src/main/java/ru/shipcollision/api/CorsConfiguration.java
@@ -12,6 +12,6 @@ public class CorsConfiguration implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowCredentials(true)
                 .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "PATCH")
-                .allowedOrigins("https://shipcollision-node.herokuapp.com", "http://localhost:5000");
+                .allowedOrigins("https://shipcollision.herokuapp.com", "http://localhost:5000");
     }
 }
