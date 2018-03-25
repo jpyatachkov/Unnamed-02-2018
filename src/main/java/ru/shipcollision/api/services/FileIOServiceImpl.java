b49d9888f96dcd689ce7d69d501e1079a0f888da
jpyatachkov <cvkucherov@yandex.ru>
2018-03-25 13:47:02 +0300

    Code style fixes

--- src/main/java/ru/shipcollision/api/services/FileIOServiceImpl.java
+++ src/main/java/ru/shipcollision/api/services/FileIOServiceImpl.java
@@ -82,8 +82,8 @@ public class FileIOServiceImpl implements FileIOService {
         }
 
         public static Path toAbsolutePath(String resoursePath) {
-            resoursePath = (resoursePath != null && resoursePath.charAt(0) == '/') ?
-                    resoursePath.substring(1) : resoursePath;
+            resoursePath =
+                    (resoursePath != null && resoursePath.charAt(0) == '/') ? resoursePath.substring(1) : resoursePath;
             return Paths.get(String.format("%s", resoursePath)).toAbsolutePath();
         }
 
